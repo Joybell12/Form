a55616742675530092a6b5861082b954a7d08725
Joybell12 <159259636+Joybell12@users.noreply.github.com>
2024-02-21 10:51:07 +0530

    Update BaseTest.java

--- src/main/java/com/Stageform/BaseTest.java
+++ src/main/java/com/Stageform/BaseTest.java
@@ -48,15 +48,14 @@ public class BaseTest {
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("enable-automation");
-        options.addArguments("--no-sandbox");
         options.addArguments("--disable-extensions");
         options.addArguments("--dns-prefetch-disable");
         options.addArguments("--disable-gpu");
         options.setExperimentalOption("useAutomationExtension",false);
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
         options.addArguments("window-size=1920,1080");
-        System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
-        System.setProperty("webdriver.chrome.verboseLogging", "true");
+        //System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
+        //System.setProperty("webdriver.chrome.verboseLogging", "true");
         /*options.addArguments('--headless');
         options.addArguments('--no-sandbox');
         options.addArguments('--disable-dev-shm-usage');*/
