c88f3ea3b8e293eb149e4b0933881946e6f25afe
Joybell12 <159259636+Joybell12@users.noreply.github.com>
2024-02-22 18:54:23 +0530

    Update BaseTest.java

--- src/main/java/com/Stageform/BaseTest.java
+++ src/main/java/com/Stageform/BaseTest.java
@@ -34,7 +34,7 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "./folder/chromedriver");
         //WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.setBinary("/usr/bin/google-chrome-stable");
+        options.setBinary("/usr/bin/google-chrome");
         //options.addArguments("--headless");
         //options.addArguments("--no-sandbox");
         //options.addArguments("enable-automation");
