958a658fa6988280608ddaf62e3e61b34f69790c
Joybell12 <159259636+Joybell12@users.noreply.github.com>
2024-03-06 14:44:30 +0530

    Update BaseTest.java

--- src/main/java/com/Stageform/BaseTest.java
+++ src/main/java/com/Stageform/BaseTest.java
@@ -43,10 +43,10 @@ public class BaseTest {
         options.addArguments("--disable-gpu");
         options.setExperimentalOption("useAutomationExtension",false);
         options.setPageLoadStrategy(PageLoadStrategy.NONE);
-        //options.addArguments("--start-maximized");
-        //options.addArguments("window-size=1920,1080");
-        //System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
-        //System.setProperty("webdriver.chrome.verboseLogging", "true");
+        options.addArguments("--start-maximized");
+        options.addArguments("window-size=1920,1080");
+        System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
      
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
