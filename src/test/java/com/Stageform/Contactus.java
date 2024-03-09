612750e772308942693b218d95e94d052b3b1b0b
Joybell12 <159259636+Joybell12@users.noreply.github.com>
2024-03-09 20:09:08 +0530

    Update Contactus.java

--- src/test/java/com/Stageform/Contactus.java
+++ src/test/java/com/Stageform/Contactus.java
@@ -16,24 +16,24 @@ public class Contactus extends BaseTest{
 
     ContactusFormSubmission() throws InterruptedException {
 
-           cntus= new Sigma(driver);
-            cntus.contactpageload();
-           cntus.contactlink();
-           Thread.sleep(3000);
-           cntus.contactpageload();
-           cntus.offer();
-           cntus.contactname(configproperities.property.getProperty("name"));
-           cntus.contactemail(configproperities.property.getProperty("emailid"));
-           cntus.contactorg(configproperities.property.getProperty("name"));
-           cntus.contactphonenumber(configproperities.property.getProperty("phoneNumber"));
-           cntus.pagedownto();
-           cntus.contactmessage(configproperities.property.getProperty("name"));
-           cntus.termsandConditions();
-           cntus.submitbutton();
-           cntus.contactpageload();
-           Thread.sleep(2000);
-           cntus.pageloadwaitforsuccessmessage();
-           cntus.cotactusvalidate();
+           //cntus= new Sigma(driver);
+            //cntus.contactpageload();
+           //cntus.contactlink();
+           //Thread.sleep(3000);
+           //cntus.contactpageload();
+           //cntus.offer();
+           //cntus.contactname(configproperities.property.getProperty("name"));
+           //cntus.contactemail(configproperities.property.getProperty("emailid"));
+           //cntus.contactorg(configproperities.property.getProperty("name"));
+           //cntus.contactphonenumber(configproperities.property.getProperty("phoneNumber"));
+           //cntus.pagedownto();
+           //cntus.contactmessage(configproperities.property.getProperty("name"));
+           //cntus.termsandConditions();
+           //cntus.submitbutton();
+           //cntus.contactpageload();
+           //Thread.sleep(2000);
+           //cntus.pageloadwaitforsuccessmessage();
+           //cntus.cotactusvalidate();
         
     }
 
